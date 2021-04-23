0bc2c9619dae458eb3f96c104e01a81a7cae43df
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-04-23 11:18:35 -0400

    fsw115Week2

---
+++ week2/apis.js
@@ -0,0 +1,32 @@
+var list = {
+    "response_code": 0,
+    "results": [
+        {
+            "category": "Entertainment: Books",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "The title of Adolf Hitler&#039;s autobiography &quot;Mein Kampf&quot; is what when translated to English?",
+            "correct_answer": "My Struggle",
+            "incorrect_answers": [
+                "My Hatred",
+                "My Sadness",
+                "My Desire"
+            ]
+        },
+        {
+            "category": "History",
+            "type": "multiple",
+            "difficulty": "medium",
+            "question": "What year did Australia become a federation?",
+            "correct_answer": "1901",
+            "incorrect_answers": [
+                "1910",
+                "1899",
+                "1911"
+            ]
+        }
+    ]
+}
+
+
+console.log(list.results[0])
