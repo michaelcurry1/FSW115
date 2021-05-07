32c3792afc2dbb257dae0c78b9a98692382269ba
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-05-07 13:56:36 -0400

    wk4

---
+++ week4/assignment1/toDo.js
@@ -0,0 +1,13 @@
+// axios
+
+
+
+axios.get(" http://api.bryanuniversity.edu/mike_curry/list/")
+.then(response => {
+    for(let i = 0; i < response.data.length; i++)
+        var h1 = document.createElement('h1')
+        console.log('h1')
+    }
+
+
+}  
