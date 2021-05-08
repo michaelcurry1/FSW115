b341839d0ff575c7d716849f691d841e87b9d9ab
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-05-07 21:36:51 -0400

    wk4

--- week4/assignment1/toDo.js
+++ week4/assignment1/toDo.js
@@ -2,13 +2,23 @@
 
 
 
-axios.get(" http://api.bryanuniversity.edu/mike_curry/list/")
-.then(response => {
-    for(let i = 0; i < response.data.length; i++)
+axios.get("http://api.bryanuniversity.edu/mike_curry/list/")
+.then(response => { console.log(response.data)
+    for(var i = 0; i < response.data.length; i++){
         var h1 = document.createElement('h1')
-        console.log('h1')
-      if()
+        h1.textContent = response.data[i].name
+       console.log(response.data.length)
+     document.body.append(h1)
+     console.log(response.data[i].isComplete)
+     if(response.data[i].isComplete === true){
+      h1.style.textDecoration = "line-through"
+      h1.style.backgroundColor = "red"
+     }
+     else if(response.data[i].isComplete === false){
+         console.log("hello")
+     }
+    }
     })
-
+    .catch(err=> console.log(err))
 
   
