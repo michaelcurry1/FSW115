29182b4de740a821c4ad943d850c3d5086e4b131
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-05-09 19:01:06 -0400

    week4..2nd

--- week4/assignment2/request.js
+++ week4/assignment2/request.js
@@ -1,2 +1,6 @@
+axios.get("http://api.bryanuniversity.edu/mike_curry/list/")
+axios.get("https://swapi.dev/api/people/1/")
+
+
 var button1 = document.getElementById(button1)
-var button1 = document.getElementById(button1)
+var button2 = document.getElementById(button2)
