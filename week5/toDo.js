10f663d48171e5fb7892915f8fe0b24dfc9de944
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-05-13 14:32:29 -0400

    wk5todos

--- week5/toDo.js
+++ week5/toDo.js
@@ -24,40 +24,40 @@
 
 //POST REQUEST......
 
-// const newTodo = {
-//     name: "test4",
-//     description:"test description 44444",
-//     price: 44,
-//     isComplete: false
-// } 
+const newTodo = {
+    name: "test4",
+    description:"test description 44444",
+    price: 44,
+    isComplete: false
+} 
 
-// const todoForm = document.todoForm
+const todoForm = document.todoForm
 
-// todoForm.addEventListener("submit", e =>{
-//     e.preventDefault()
+todoForm.addEventListener("submit", e =>{
+    e.preventDefault()
 
-//     const newTodo = {
-//         name: todoForm.name.value,
-//         description: todoForm.description.value,
-//         price: todoForm.description.value,
+    const newTodo = {
+        name: todoForm.name.value,
+        description: todoForm.description.value,
+        price: todoForm.description.value,
         
-//     }
+    }
     
-//     axios.post("http://api.bryanuniversity.edu/mike_curry/list/",newTodo)
-//     .then(res => console.log(res))
-//     .catch(err => console.log(err))
+    axios.post("http://api.bryanuniversity.edu/mike_curry/list/",newTodo)
+    .then(res => console.log(res))
+    .catch(err => console.log(err))
 
-// });
+});
 
 // PUT REQUEST
 
-// const updates ={
-//     name:"THIS HAS BEEN UPDATED"
-// }
+const updates ={
+    name:"THIS HAS BEEN UPDATED"
+}
 
-// axios.put("http://api.bryanuniversity.edu/mike_curry/list/5500401b-13f0-4322-adfc-af3684b0a5e0", updates)
-//     .then(res => console.log(res))
-//     .catch(err => (console.log(err))
+axios.put("http://api.bryanuniversity.edu/mike_curry/list/5500401b-13f0-4322-adfc-af3684b0a5e0", updates)
+    .then(res => console.log(res))
+    .catch(err => (console.log(err))
 
 
 
