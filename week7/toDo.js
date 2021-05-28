b9a3a96213a930eb4796466c1a0d9147d590e42f
Your usernamemichaelcurry1 <michael.curry@bryanuniversity.edu>
2021-05-27 21:31:51 -0400

    week7

--- week7/toDo.js
+++ week7/toDo.js
@@ -22,24 +22,24 @@ axios.get("http://api.bryanuniversity.edu/mike_curry/list/")
     .catch(err=> console.log(err))
 
     
-axios.get("http://api.bryanuniversity.edu/mikecurry/list/")
-.then(response => { console.log(response.data)
-    for(var i = 0; i < response.data.length; i++){
-        var h1 = document.createElement('h1')
-        h1.textContent = response.data[i].name
-       console.log(response.data.length)
-     document.body.append(h1)
-     console.log(response.data[i].isComplete)
-     if(response.data[i].isComplete === true){
-      h1.style.textDecoration = "line-through"
-      h1.style.backgroundColor = "red"
-     }
-     else if(response.data[i].isComplete === false){
-         console.log("hello")
-     }
-    }
-    })
-    .catch(err=> console.log(err))
+// axios.get("http://api.bryanuniversity.edu/mikecurry/list/")
+// .then(response => { console.log(response.data)
+//     for(var i = 0; i < response.data.length; i++){
+//         var h1 = document.createElement('h1')
+//         h1.textContent = response.data[i].name
+//        console.log(response.data.length)
+//      document.body.append(h1)
+//      console.log(response.data[i].isComplete)
+//      if(response.data[i].isComplete === true){
+//       h1.style.textDecoration = "line-through"
+//       h1.style.backgroundColor = "red"
+//      }
+//      else if(response.data[i].isComplete === false){
+//          console.log("hello")
+//      }
+//     }
+//     })
+//     .catch(err=> console.log(err))
 
 
 //POST REQUEST......
